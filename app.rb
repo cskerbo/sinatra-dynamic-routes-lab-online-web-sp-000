5aa34416557329c08dc96c15c938927b12f167d2
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-08 23:15:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,9 +25,9 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
-    @number1.to_i = params[:number1]
-    @number2.to_i = params[:number2]
-    @result = @number1 + "#{@operation}" + @number2
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    @number1.to_i + "#{@operation}" + @number2.to_i
     "#{@result}"
   end
 
