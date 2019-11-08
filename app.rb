16196361a0eeb7407636da05b2374b25b903986d
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-08 23:03:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,4 +23,11 @@ class App < Sinatra::Base
     "#{@string.join(" ")}."
   end
 
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    "#{@number @operation @number2}"
+  end
+
 end
