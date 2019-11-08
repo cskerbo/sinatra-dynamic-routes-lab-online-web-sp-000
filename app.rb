dd0a82d401b41c0ed2ae9f8e69b0b2a571d88a8f
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-08 23:39:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,6 +33,8 @@ class App < Sinatra::Base
         "#{@number1.to_i - @number2.to_i}"
     elsif @operation == "multiply"
         "#{@number1.to_i * @number2.to_i}"
+    elsif @operation == "divide"
+        "#{@number1.to_i / @number2.to_i}"
     end
   end
 
