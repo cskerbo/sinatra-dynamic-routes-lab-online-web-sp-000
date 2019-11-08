36fa369e4001b02e8ce3f0bb9f2666dc7875505a
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-08 22:58:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @string = params[:word1] + params[:word2]
+    @string = params[:word1 ] + params[:word2]
     "#{@string}"
   end
 
