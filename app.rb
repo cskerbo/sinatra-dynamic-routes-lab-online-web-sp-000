09a077a83e79083ddce228b5b2732a2a6de51a46
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-08 22:45:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/reversename/:name' do
     @name = params[:name]
     "#{@name.reverse!}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number] ** 2
+    "#{@number}"
   end
 
 end
